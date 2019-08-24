4da8d40fb2860a14bce61b9176182a18b9471e77
perylemke <pery.lemke@gmail.com>
2019-08-23 22:26:43 -0300

    Adjust year

--- src/apery_test.go
+++ src/apery_test.go
@@ -36,7 +36,7 @@ func TestHelloWorld(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	expected := fmt.Sprintf("Olá %s! Bem vindo ao Floripa Tech Day 2019! :)", name)
+	expected := fmt.Sprintf("Olá %s! Bem vindo ao Floripa Tech Day 2018! :)", name)
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
